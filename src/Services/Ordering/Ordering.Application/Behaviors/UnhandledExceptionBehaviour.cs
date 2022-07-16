450c21843838ccafadf4a8ac3d6423999dd5c2ba
Damian Kucharski <kuchar187@gmail.com>
2022-07-16 17:05:41 +0200

    Add ordering

---
+++ src/Services/Ordering/Ordering.Application/Behaviors/UnhandledExceptionBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Behaviors
+{
+    public class UnhandledExceptionBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        private readonly ILogger _logger;
+
+        public UnhandledExceptionBehaviour(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            try
+            {
+                return await next();
+            }
+            catch (Exception ex)
+            {
+                var requestName = typeof(TRequest).Name;
+                _logger.LogError(ex, $"App request: Unhandled exception for request {requestName}");
+                throw;
+            }
+        }
+    }
+}
